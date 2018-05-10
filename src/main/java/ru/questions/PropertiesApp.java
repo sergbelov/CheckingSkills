c8c4216bc5807548311d90283c7b0bab007273aa
Sergey Belov <sabelov@rambler.ru>
2018-05-10 20:19:49 +0300

    CheckingSkills

--- src/main/java/ru/questions/PropertiesApp.java
+++ src/main/java/ru/questions/PropertiesApp.java
@@ -112,10 +112,6 @@ public class PropertiesApp {
         }
     }
 
-    public static Logger getLOG() {
-        return LOG;
-    }
-
     public int getMAX_QUESTION() {
         return MAX_QUESTION;
     }
